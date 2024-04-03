2bab831ccc1c2dd458cf6cb04589b81a83c94591
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-03 22:08:42 +0200

    prueba

---
+++ pruebas.py
@@ -0,0 +1,26 @@
+# Tipos de datos 
+
+# Tipos númericos float, int
+import math
+numero_float = -5.0
+numero_int = 5
+# print(math.sqrt(9))
+# print(divmod(7,3))
+
+
+# Tipos númericos booleanos, True y False
+num = 7
+# print(num == 7) -> True
+num = 11
+# print(num >= 1 and num <= 10) -> False
+
+
+# Entrada y Salida Estándar :  print || input
+
+# Introducción a la cadena de caracteres, comparación y operaciones
+# cad1 = 'prueba'
+# cad2 = "prueba"
+# cad3 = '''Hola
+# como estás?'''
+
+
