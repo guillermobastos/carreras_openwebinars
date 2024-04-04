39de68f829a79f59d93d56e277f2032f3c9740f2
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-04 19:58:52 +0200

    update

---
+++ fundamentos_ia/README.md
@@ -0,0 +1,18 @@
+# Definición de IA
+
+## Visión Artificial y Sistemas de Recomendación
+Caso de Netflix y las diferentes portadas dependiendo
+de las preferencias del usuario
+
+## Inteligencias Artificial para las Finanzas
+
+
+##  Inteligencias Artificial en el Comercio
+Predicción de demanda
+Marketing y publicidad
+
+##  Inteligencias Artificial en las Plataformas Digitales
+Sugerencias de contenido 
+
+
+
---
+++ python_inicio/ejercicios/pruebas.py
@@ -0,0 +1,45 @@
+# Tipos de datos 
+
+# Tipos númericos float, int
+import math
+numero_float = -5.0
+numero_int = 5
+# print(math.sqrt(9))
+# print(divmod(7,3))
+
+# -------------------------------------------------------------------
+# Tipos númericos booleanos, True y False
+num = 7
+# print(num == 7) -> True
+num = 11
+# print(num >= 1 and num <= 10) -> False
+
+# -------------------------------------------------------------------
+# Entrada y Salida Estándar :  print || input
+
+# Introducción a la cadena de caracteres, comparación y operaciones
+# cad1 = 'prueba'
+# cad2 = "prueba"
+# cad3 = '''Hola
+# como estás?'''
+# "prueba" == "prueba" -> True
+
+# -------------------------------------------------------------------
+# Estructuras repetitivas
+
+# Uso del While
+# secreto = "1234"
+# clave = input("Dime la clave: ")
+# while clave != secreto:
+#     print("Contraseña erronea") 
+#     clave = input("Dime la clave: ")
+# print("Contraseña correcta")
+
+# Uso del For
+# for var in range(1,20):
+#     print(var,"",end="")
+
+# for var in range(10,0,-1):
+#     print(var,"",end="")
+
+# -------------------------------------------------------------------
