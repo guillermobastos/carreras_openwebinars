694645581bf9b174b0dd52b9612a7758085810cf
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-06 17:38:49 +0200

    update

---
+++ python_inicio/ejercicios/tipos_datos_secuenciales.py
@@ -0,0 +1,13 @@
+# Cadenas
+cadena = "informática"
+print("a" in cadena) 
+print(cadena[0])
+print(cadena[2:5])
+print(cadena[2:7:2]) #desde la posición 2 a la 6 de dos en dos
+print(cadena[2:]) # desde la posición 2 al final
+print(cadena[:5]) # desde el principio a la posición 4
+print(cadena[::-1]) # cadena invertida 
+print(cadena.upper())
+# Objetos de tipo cadena son Inmutables
+
+
