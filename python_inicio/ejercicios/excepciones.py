3db3fc930e8418a196f8f994c24b43453f4eed49
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-10 16:58:51 +0200

    Update excepciones.py

--- python_inicio/ejercicios/excepciones.py
+++ python_inicio/ejercicios/excepciones.py
@@ -10,4 +10,26 @@
 # a + 4
 # NameError: name 'a' is not defined
 
-"4" +4
+# "4" + 4
+# TypeError: can only concatenate str (not "int") to str
+
+# Ejemplo 1
+while True:
+    try:
+        x= int(input("Ingrese un número: "))
+        break
+    except ValueError:
+        print("No es un número")    
+        
+# Ejemplo 2
+cad=input("Ingrese una número: ")
+try:
+    print(10/int(cad))
+except ValueError:
+    print("No se puedo convertir a entero")
+except ZeroDivisionError:
+    print("No se puedo dividir por cero")        
+else:
+    print("Se ha producido otro error")
+finally:
+    print("Se ejecuta siempre al final")
