2a2cbdb5d36d457ad70ccfd046324da4e5e1b484
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-10 17:00:57 +0200

    update

--- trabajo.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-df1 = pd.read_excel('C:/Users/Usuario/Documents/trabajo/Tabla1.xlsx')
-df2 = pd.read_excel('C:/Users/Usuario/Documents/trabajo/Tabla2.xlsx')
-
-
-df_merged = pd.merge(df1, df2, on=['Dia', 'Mes', 'Ano'])
-df_sin_duplicados = df_merged.drop_duplicates(subset=['Dia','Mes','Ano'])
-df_sin_duplicados.to_excel('C:/Users/Usuario/Documents/trabajo/Tabla_Final_Sin_Duplicados.xlsx',index=False)
