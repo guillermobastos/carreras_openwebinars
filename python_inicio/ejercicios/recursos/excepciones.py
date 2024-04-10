d16c62d9be7913b3f578eb2a88b28a8c6c8266ac
guillermobastos <bastosribasguillermo@gmail.com>
2024-04-10 12:41:43 +0200

    Add files via upload

---
+++ python_inicio/ejercicios/recursos/excepciones.py
@@ -0,0 +1,13 @@
+# while True print("Prueba excepciones")
+# File "c:\Users\guille\Documents\Trabajo\Proyecto\excepciones.py", line 1
+#     while True print("Prueba excepciones")
+#                ^^^^^
+# SyntaxError: invalid syntax
+
+# 4 / 0
+# ZeroDivisionError: division by zero
+
+# a + 4
+# NameError: name 'a' is not defined
+
+"4" +4
